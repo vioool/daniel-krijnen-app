3bdd2194a3cadb516fbed916ddaa7cbf26decdf0
Viola Puhalovic-Krijnen <violapuhalovic@gmail.com>
2022-04-15 16:58:48 +0200

    DK-005-button work in progress

---
+++ src/components/DKButton.vue
@@ -0,0 +1,43 @@
+<template>
+  <button class="dk-button">
+    <slot>
+      Click Me!
+    </slot>
+  </button>
+</template>
+
+<script>
+
+</script>
+
+<style>
+button {
+  display: inline-block;
+  border: none;
+  padding: 1rem 2rem;
+  margin: 0;
+  text-decoration: none;
+  background: var(--color-grey-light);
+  color: var(--color-lightest);
+  cursor: pointer;
+  text-align: center;
+  transition: background 250ms ease-in-out, transform 150ms ease;
+  -webkit-appearance: none;
+  -moz-appearance: none;
+}
+
+button:hover,
+button:focus {
+  background: var(--color-grey);
+}
+
+button:focus {
+  outline: solid var(--color-lightest) .0125rem;
+  outline-offset: -.25rem;
+}
+
+button:active {
+  transform: scale(0.99);
+}
+
+</style>
